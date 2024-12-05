341dd7e48d174c6a400952cec6ff94da88ca2328
Travis Hervey <thervey@predictiveindex.com>
2024-12-05 18:46:51 -0500

    Day 1

---
+++ AdventOfCode2024/Day01.cs
@@ -0,0 +1,47 @@
+﻿using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace AdventOfCode2024;
+
+internal sealed class Day01 : AsyncCommand
+{
+    public override async Task<int> ExecuteAsync(CommandContext context)
+    {
+        var data = await File.ReadAllLinesAsync("./DataFiles/Day01.txt");
+        var locationListA = new List<int>();
+        var locationListB = new List<int>();
+
+        foreach (var line in data)
+        {
+            var numberPairs = line.Split("   ");
+            locationListA.Add(int.Parse(numberPairs[0]));
+            locationListB.Add(int.Parse(numberPairs[1]));
+        }
+
+        locationListA.Sort();
+        locationListB.Sort();
+
+        var listDiff = new List<int>();
+
+        for (int i = 0; i < locationListA.Count; i++)
+        {
+            listDiff.Add(int.Abs(locationListA[i] - locationListB[i]));
+        }
+
+        var part1Answer = listDiff.Sum();
+        
+        AnsiConsole.MarkupLineInterpolated($"[bold red]Part 1:[/] {part1Answer}");
+
+        var similarityScores = (
+            from location in locationListA 
+            let occurrencesCnt = locationListB.Count(x => x == location) 
+            select location * occurrencesCnt
+        ).ToList();
+
+        var part2Answer = similarityScores.Sum();
+        
+        AnsiConsole.MarkupLineInterpolated($"[bold green]Part 2:[/] {part2Answer}");
+        
+        return 0;
+    }
+}
---
+++ AdventOfCode2024/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AdventOfCode2024;
+using Spectre.Console.Cli;
+
+var type = typeof(Day01);
+
+var appType = typeof(CommandApp<>).MakeGenericType(type);
+var app = (ICommandApp)Activator.CreateInstance(appType) ?? throw new InvalidOperationException("Failed to create CommandApp");
+
+return app.Run(args);
