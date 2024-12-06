c0aa97f3a8d51154ccb0d520dc3640039fb35286
travis <thervey@gmail.com>
2024-12-06 08:29:01 -0500

    Day 2, Part 2

--- AdventOfCode2024/Day02.cs
+++ AdventOfCode2024/Day02.cs
@@ -5,51 +5,74 @@ namespace AdventOfCode2024;
 
 internal sealed class Day02 : AsyncCommand
 {
-    public Day02()
-    {
-        
-    }
-    
     public override async Task<int> ExecuteAsync(CommandContext context)
     {
         var data = await File.ReadAllLinesAsync($"./DataFiles/{nameof(Day02)}.txt");
 
         var safeReports = 0;
         var line = 1;
+        var problemReports = new List<string>();
         foreach (var report in data)
         {
             AnsiConsole.MarkupInterpolated($"{line}: [Gray] {report}[/]");
-            var reportValues = report.Split(" ");
-            bool isIncreasing;
-            bool? previousValue = null;
-            bool isSafe = true;
-            for (int i = 1; i < reportValues.Length; i++)
-            {
-                var reportValueA = int.Parse(reportValues[i]);
-                var reportValueB = int.Parse(reportValues[i-1]);
-             
-                isIncreasing = reportValueA < reportValueB;
-                
-                var diff = Math.Abs(reportValueA - reportValueB);
-                if (diff > 3 || (previousValue ?? isIncreasing) != isIncreasing || reportValueA == reportValueB)
-                    isSafe = false;
-                previousValue = isIncreasing;
-            }
-
-            if (isSafe)
+            var reportValues = report.Split(" ").ToList();
+            
+            if (IsReportSafe(reportValues))
             {
                 AnsiConsole.MarkupLine($" [bold Green] Passed[/]");
                 safeReports++;
             }
             else
             {
-                AnsiConsole.MarkupLine($" [bold Red] Failed[/]");
+                problemReports.Add(report);
+                AnsiConsole.MarkupLine($" [Yellow] Problem Detected[/]");
+                //AnsiConsole.MarkupLine($" [bold Red] Failed[/]");
             }
             line++;
         }
         
         AnsiConsole.MarkupLineInterpolated($"[bold red]Part 1:[/] {safeReports}");
+
+        foreach (var report in problemReports)
+        {
+            var reportValues = report.Split(" ").ToList();
+            for (int i = 0; i < reportValues.Count; i++)
+            {
+                var temp = new string[reportValues.Count];
+                reportValues.CopyTo(0, temp,0, reportValues.Count);
+                var list = temp.ToList();
+                list.RemoveAt(i);
+                if (IsReportSafe(list))
+                {
+                    AnsiConsole.MarkupLineInterpolated($"{report} made safe by removing report {i+1}");
+                    safeReports++;
+                    break;
+                }
+            }
+        }
+        
+        AnsiConsole.MarkupLinceInterpolated($"[bold Green]Part 2:[/] {safeReports}");
         
         return 0;
+    }
+
+    private bool IsReportSafe(List<string> reportValues)
+    {
+        bool isIncreasing;
+        bool? previousValue = null;
+        bool isSafe = true;
+        for (int i = 1; i < reportValues.Count; i++)
+        {
+            var reportValueA = int.Parse(reportValues[i]);
+            var reportValueB = int.Parse(reportValues[i-1]);
+             
+            isIncreasing = reportValueA < reportValueB;
+                
+            var diff = Math.Abs(reportValueA - reportValueB);
+            if (diff > 3 || (previousValue ?? isIncreasing) != isIncreasing || reportValueA == reportValueB)
+                isSafe = false;
+            previousValue = isIncreasing;
+        }
+        return isSafe;
     }
 }
