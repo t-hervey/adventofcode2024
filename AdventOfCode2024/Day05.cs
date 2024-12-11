7e77fb88a537f4fb98f0f467a8c4077e85bf8ec2
Travis Hervey <thervey@predictiveindex.com>
2024-12-11 11:13:37 -0500

    Day 5 setup

---
+++ AdventOfCode2024/Day05.cs
@@ -0,0 +1,11 @@
+﻿using Spectre.Console.Cli;
+
+namespace AdventOfCode2024;
+
+internal sealed class Day05 : AsyncCommand
+{
+    public override Task<int> ExecuteAsync(CommandContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
